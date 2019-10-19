cc0b2cf6786d91b350634564fd8fb6466945a867
shawavisek35 <shawavisek35@gmail.com>
2019-10-19 10:45:22 +0530

    app.py updated

--- app/app.py
+++ app/app.py
@@ -6,6 +6,7 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
 kivy.require("1.11.1")
 #my app will inherit from the main App class in kivy
@@ -14,9 +15,9 @@ class ConnectPage(GridLayout):#ConnectPage class is inheriting from Grid Layout 
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         self.cols = 2
-        self.add_widget(Label(text="IP : "))
-        self.ip = TextInput(multiline = False)
-        self.add_widget(self.ip)
+        self.add_widget(Label(text="IP : "))#for adding a textview
+        self.ip = TextInput(multiline = False)#for adding edit text
+        self.add_widget(self.ip)#taking input
 
         self.add_widget(Label(text="Port : "))
         self.port = TextInput(multiline = False)
@@ -25,6 +26,19 @@ class ConnectPage(GridLayout):#ConnectPage class is inheriting from Grid Layout 
         self.add_widget(Label(text="Username : "))
         self.username = TextInput(multiline = False)
         self.add_widget(self.username)
+
+        self.join = Button(text = "join")
+        self.join.bind(on_press = self.join_button)#for binding a function to the button
+        self.add_widget(Label())
+        self.add_widget(self.join)
+
+    def join_button(self,instance):
+        #grabbing the values inserted by the user in the above method
+        port = self.port.text
+        ip = self.ip.text
+        name = self.username.text
+
+        print(f"Attempting to join : {ip} : {port} as {name}")
 
 class EpicApp(App):
     def build(self):#initialization method
